f4868fc276c2b3f8ac1d3b2fac0732ace7057b9c
ishanhimalka <ishanhimalka1@gmail.com>
2022-06-18 06:26:21 +0530

    setup navigation bar design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import HomePage from "./View/HomePage";
+import Header from "./Common/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <Router>
         <Routes>
-          {/*<Route path={"/"} element={<Login/>}/>*/}
+          <Route path={"/"} element={<HomePage/>}/>
         </Routes>
       </Router>
     </div>
